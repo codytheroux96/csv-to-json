d9a48ecc972cc24165b7e1805dcb6cadeef7cdc0
Cody Theroux <cody.theroux3@gmail.com>
2023-12-18 19:48:55 -0500

    made functions to process lines and then process the csv file

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 )
@@ -17,6 +19,12 @@ type inputFile struct {
 func exitGracefully(err error) {
 	fmt.Fprintf(os.Stderr, "error: %v\n", err)
 	os.Exit(1)
+}
+
+func check(e error) {
+	if e != nil {
+		exitGracefully(e)
+	}
 }
 
 func getFileData() (inputFile, error) {
@@ -43,10 +51,55 @@ func checkIfValidFile(filename string) (bool, error) {
 	if fileExtenstion := filepath.Ext(filename); fileExtenstion != ".csv" {
 		return false, fmt.Errorf("File %s is not a CSV file", filename)
 	}
-	 if _, err := os.Stat(filename); err != nil && os.IsNotExist(err) {
+	if _, err := os.Stat(filename); err != nil && os.IsNotExist(err) {
 		return false, fmt.Errorf("File %s does not exist!", filename)
-	 }
-	 return true, nil
+	}
+	return true, nil
+}
+
+func processLine(headers []string, dataList []string) (map[string]string, error) {
+	if len(dataList) != len(headers) {
+		return nil, errors.New("Line does not match headers format. Going to skip.")
+	}
+	recordMap := make(map[string]string)
+
+	for i, name := range headers {
+		recordMap[name] = dataList[i]
+	}
+	return recordMap, nil
+}
+
+func processCsvFile(fileData inputFile, writerChannel chan<- map[string]string) {
+	file, err := os.Open(fileData.filepath)
+	check(err)
+	defer file.Close()
+
+	var headers, line []string
+	reader := csv.NewReader(file)
+	if fileData.separator == "semicolon" {
+		reader.Comma = ';'
+	}
+
+	headers, err = reader.Read()
+	check(err)
+
+	for {
+		line, err = reader.Read()
+		if err == io.EOF {
+			close(writerChannel)
+			break
+		} else if err != nil {
+			exitGracefully(err)
+		}
+		record, err := processLine(headers, line)
+
+		if err != nil {
+			fmt.Printf("Line: %sError: %s\n", line, err)
+			continue
+		}
+
+		writerChannel <- record
+	}
 }
 
 func main() {
